4c82e467c687d058525e7739180f1396062c2b9d
Đặng Mai Anh Tú <tudmase182951@fpt.edu.vn>
2025-06-21 19:25:40 +0700

    Finish login and UI tag feature

--- FUNewsRazorPages/FUNewsRazorPages/Pages/Tags/Index.cshtml.cs
+++ FUNewsRazorPages/FUNewsRazorPages/Pages/Tags/Index.cshtml.cs
@@ -1,5 +1,7 @@
 using FuNews.BLL.Interface;
+using FuNews.Modals.DTOs.Request;
 using FuNews.Modals.DTOs.Response;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FUNewsRazorPages.Pages.Tags;
@@ -15,8 +17,38 @@ public class IndexModel : PageModel
 
     public List<TagResponse> Tags { get; set; } = new();
 
+    [BindProperty]
+    public TagRequest Tag { get; set; } = new();
+
     public async Task OnGetAsync()
     {
         Tags = await _tagService.GetAllTagsAsync();
+    }
+
+    // Handle Create
+    public async Task<IActionResult> OnPostCreateAsync()
+    {
+        if (!ModelState.IsValid)
+            return Page();
+
+        await _tagService.AddTagAsync(Tag);
+        return RedirectToPage();
+    }
+
+    // Handle Edit
+    public async Task<IActionResult> OnPostEditAsync(int id)
+    {
+        if (!ModelState.IsValid)
+            return Page();
+
+        await _tagService.UpdateTagAsync(id, Tag);
+        return RedirectToPage();
+    }
+
+    // Handle Delete
+    public async Task<IActionResult> OnPostDeleteAsync(int id)
+    {
+        await _tagService.DeleteAsync(id);
+        return RedirectToPage();
     }
 }
